fa161630b1c39bc22aae0c2c9500ce3e2f61d593
GeePawHill <geepawhill@geepawhill.org>
2020-05-21 11:29:21 -0400

    First nonsense test and run configurations added.

---
+++ src/test/kotlin/org/geepawhill/nonagrams/SomethingTest.kt
@@ -0,0 +1,11 @@
+package org.geepawhill.nonagrams
+
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+class SomethingTest {
+    @Test
+    fun `passing test`() {
+        assertThat(1).isEqualTo(1)
+    }
+}
