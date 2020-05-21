ce9ad234b18dfc8cc11994108f59ad20cd5a3376
GeePawHill <geepawhill@geepawhill.org>
2020-05-21 12:34:11 -0400

    Three panes with two splitters.

--- src/main/kotlin/org/geepawhill/nonagrams/Main.kt
+++ src/main/kotlin/org/geepawhill/nonagrams/Main.kt
@@ -1,10 +1,23 @@
 package org.geepawhill.nonagrams
 
+import javafx.geometry.Orientation
 import tornadofx.*
 
 class Main : App(MainView::class)
 
 class MainView : View() {
-	override val root = borderpane { }
+	override val root = splitpane {
+		splitpane(Orientation.VERTICAL) {
+			stackpane {
+				text("This should be top left")
+			}
+			stackpane {
+				text("This should be bottom left")
+			}
+		}
+		stackpane {
+			text("This should be right")
+		}
 	}
+}
 	
